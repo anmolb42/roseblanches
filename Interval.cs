1f3ec131b6eb1cbeed846c18957cc0a4f0ef35a2
Anmol <budhathokianmol75@gmail.com>
2024-02-12 15:04:18 -0500

    Final commit

---
+++ Interval.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interval
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter anything to start and end to exit");
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+
+                Console.WriteLine("Enter a real number: ");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                bool belongsI = (x >= 2 && x < 3) || (x >= 0 && x <= 1) || (x >= -10 && x <= -2);
+
+                if (belongsI)
+                {
+                    Console.WriteLine("x belongs to I");
+
+                }
+                else
+                {
+                    Console.WriteLine("x does not belong to I");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RoseBlanches.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("How much did you receive money ($)? ");
+            double moneyReceived = Convert.ToDouble(Console.ReadLine());
+
+            double booksAndSupplies = moneyReceived * 0.75;
+            double remainingMoney = moneyReceived - booksAndSupplies;
+
+            double dividedMoney = remainingMoney / 3;
+
+            double coffee = (dividedMoney / 2);//dividing by 2 because cost of coffee is 2 dollars
+            double change1 = (dividedMoney % 2);//collecting change for the white roses
+
+            double computer = (dividedMoney / 4);//cost of computer is 4 dollars
+            double change2 = (dividedMoney % 4);
+
+            double subway = (dividedMoney / 3);//cost of tickets is 3 dollars
+            double change3 = (dividedMoney % 3);
+
+            Console.WriteLine("Book and Supplies: " + booksAndSupplies + " $");
+            Console.WriteLine("You can then buy: ");
+         
+            // Math.floor is added to round off the values.
+            //If we add it above while dividing then it will affect output.//
+            
+            Console.WriteLine(Math.Floor(coffee) + " coffees");
+            Console.WriteLine(Math.Floor(computer) + " Flash Computer Numbers");
+            Console.WriteLine(Math.Floor(subway) + " Subway Tickets");
+            Console.WriteLine("and you will have " + Math.Floor(change1 + change2 + change3) + " dollars for the white roses.");
+
+
+            Console.Read();
+        }
+    }
+
+}
